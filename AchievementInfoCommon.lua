574619e79440004b21d16f8827d0c228b0eeb8eb
Florian Brinker <fbrinker@users.noreply.github.com>
2016-08-04 11:10:23 +0200

    Updated Version

--- AchievementInfoCommon.lua
+++ AchievementInfoCommon.lua
@@ -8,7 +8,7 @@
 AchievementInfo             = {}
 AchievementInfo.name        = "AchievementInfo"
 AchievementInfo.author      = "Asto, @Astarax"
-AchievementInfo.version     = 2.8
+AchievementInfo.version     = 2.9
 AchievementInfo.savedVars   = nil
 AchievementInfo.LangStore   = {}
 
