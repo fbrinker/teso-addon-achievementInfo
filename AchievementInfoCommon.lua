af0c316a4d8bc22d096e1b0e387fe2c18ac3759c
Florian Brinker <fbrinker@users.noreply.github.com>
2016-08-07 09:13:50 +0200

    French translation

--- AchievementInfoCommon.lua
+++ AchievementInfoCommon.lua
@@ -8,7 +8,7 @@
 AchievementInfo             = {}
 AchievementInfo.name        = "AchievementInfo"
 AchievementInfo.author      = "Asto, @Astarax"
-AchievementInfo.version     = 2.9
+AchievementInfo.version     = 2.10
 AchievementInfo.savedVars   = nil
 AchievementInfo.LangStore   = {}
 
