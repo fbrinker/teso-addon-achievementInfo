a85ddcf3051e8422b9739cffc581fa792f713796
Florian Brinker <fbrinker@users.noreply.github.com>
2017-11-08 20:36:28 +0100

    Api Version 100021

--- AchievementInfoCommon.lua
+++ AchievementInfoCommon.lua
@@ -8,7 +8,7 @@
 AchievementInfo             = {}
 AchievementInfo.name        = "AchievementInfo"
 AchievementInfo.author      = "Asto, @Astarax"
-AchievementInfo.version     = 2.14
+AchievementInfo.version     = 2.15
 AchievementInfo.savedVars   = nil
 AchievementInfo.LangStore   = {}
 
